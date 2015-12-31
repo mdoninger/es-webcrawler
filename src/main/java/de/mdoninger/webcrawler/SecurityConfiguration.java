0d975528b801ef12926af0fae4e9ca25a6a5c1d3
Manuel Doninger <manuel.doninger@gmail.com>
2015-12-31 19:52:43 +0100

    Added permit all security configuratino

--- src/main/java/de/mdoninger/webcrawler/SecurityConfiguration.java
+++ src/main/java/de/mdoninger/webcrawler/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package de.mdoninger.webcrawler;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -12,5 +13,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll();
     }
 }
