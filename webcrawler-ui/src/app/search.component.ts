1d42838083a18483bb461518b1d5613ed2da20ec
Manuel Doninger <manuel.doninger@gmail.com>
2016-01-02 19:17:30 +0100

    Query boot backend for search service

--- webcrawler-ui/src/app/search.component.ts
+++ webcrawler-ui/src/app/search.component.ts
@@ -1,6 +1,7 @@
 import {Component} from "angular2/core";
 import {SearchService} from "./search.service";
 import {SearchResult} from "./searchresult";
+import {Response} from "angular2/http";
 
 @Component({
     selector: 'search-app',
@@ -9,14 +10,17 @@ import {SearchResult} from "./searchresult";
 })
 export class SearchComponent {
     public searchResult:SearchResult[];
+    public searchResultString:string;
     public title = "Welcome to the Webcrawler Search";
     public searchInput:string;
+    public error:string;
 
     constructor(private _searchService:SearchService) {
     }
 
     submitQuery() {
-        this._searchService.search(this.searchInput).then(result => this.searchResult = result);
+        //this._searchService.search(this.searchInput).then(result => this.searchResult = result);
+        this._searchService.search(this.searchInput).subscribe((response:Response) => this.searchResult = response.json()["content"]);
     }
 
 }
--- webcrawler-ui/src/app/search.service.ts
+++ webcrawler-ui/src/app/search.service.ts
@@ -1,15 +1,22 @@
 import {Injectable} from 'angular2/core';
-//import {Http} from 'angular2/http';
-import {RESULT} from './mock-result';
+import {Http, RequestOptions, URLSearchParams} from 'angular2/http';
 
 @Injectable()
 export class SearchService {
 
-    //constructor(private _http:Http) {
-    //}
+    constructor(private _http:Http) {
+    }
 
     search(searchQuery) {
         //return Promise.resolve(this._http.get("http://localhost:8080/search?q=" + searchQuery).map(res => res.json()));
-        return Promise.resolve(RESULT);
+
+        const params = new URLSearchParams();
+        params.set("q", searchQuery);
+
+        var result = this._http.get("http://localhost:8080/search", new RequestOptions({search: params}));
+
+        console.log(result);
+
+        return result;
     }
 }
