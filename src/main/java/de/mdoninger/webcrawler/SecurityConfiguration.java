6743cc45e141965b5e53831a72565e5593b4976e
Manuel Doninger <manuel.doninger@gmail.com>
2016-01-02 19:15:49 +0100

    Added security configuration for allow origin header

--- src/main/java/de/mdoninger/webcrawler/SecurityConfiguration.java
+++ src/main/java/de/mdoninger/webcrawler/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
 
 /**
  * @author Manuel Doninger
@@ -17,6 +18,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+        // @formatter:off
+        http.
+                authorizeRequests().anyRequest().permitAll().and().
+                headers().
+                    addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "*"));
+
+        // @formatter:on
     }
 }
